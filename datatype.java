429906b6bf644986e34eb012e04a3ed2d9710411
LOKESHWARAN.S <94963133+lokeshwaran173@users.noreply.github.com>
2024-02-06 09:55:06 +0530

    Create datatype.java

---
+++ datatype.java
@@ -0,0 +1,19 @@
+class loki
+
+{
+    public static void main(String args[])
+    {
+        char a = 'G';
+        int i = 86;
+        byte b = 55;
+        short s = 52;
+        double d = 4.352;
+        
+        System.out.println("char:" +a);
+        System.out.println("integer:" +i);
+        System.out.println("byte": +b);
+        System.out.println("short": +s);
+        System.out.println("double": +d);
+    
+    }
+}
