7519939aec325e98b6ba4d45b5e18c629ece1102
LOKESHWARAN.S <94963133+lokeshwaran173@users.noreply.github.com>
2024-02-07 12:00:30 +0530

    Create covert dt.java

---
+++ covert dt.java
@@ -0,0 +1,11 @@
+public class main
+{
+    public static void main(String args[])
+    {
+        double mydouble = 9.78d;
+        int myint = (int)mydouble;
+        
+        System.out.println(mydouble);
+        System.out.println(myint);
+    }
+}
